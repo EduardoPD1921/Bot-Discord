6368a45e6f5e57d137e7184d34d316715b408ce1
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-06-10 14:26:29 -0300

    Pequenas novas funcionalidades

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
-const token = 'NzIwMzE0NTM1OTUyNzc3Mjg3.XuELnA.cikqmLSK6wC8cd_AbDXB3gJptaY';
+const token = 'NzIwMzE0NTM1OTUyNzc3Mjg3.XuEQSQ.-7UcPpDNTNV1Xt36JWls8tp6oMg';
 
 bot.login(token);
 bot.on('ready', () => {
     console.log('Bot On');
 })
+
+let dSize = 0;
+
 bot.on('message', msg => {
     if (msg.content == 'salve japa') {
-        msg.reply('salve, meu pau acabou de crescer mais 8km e neste instante está passando a estratosfera');
+        dSize++;
+        msg.reply(`salve, meu pau acabou de crescer mais 1km estando com ${dSize}km de pura massa de pênis.`);
+    } else if (msg.content == 'japa status') {
+        msg.reply(`O tamanho atual do meu pênis é ${dSize}km`);
     }
 })
