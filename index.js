a29097caaf53cae7f4bef4ac3c5dfeca4ef36467
Eduardo Gomes Heleno <Eduardo.gheleno@gmail.com>
2020-06-10 15:40:59 -0300

    try catch + split slice

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const discord = require('discord.js');
 const bot = new discord.Client();
-const token = 'NzIwMzE0NTM1OTUyNzc3Mjg3.XuEQSQ.-7UcPpDNTNV1Xt36JWls8tp6oMg';
+const token = 'NzIwMzE0NTM1OTUyNzc3Mjg3.XuEkfg.J3wR1nKYegwawgcg6t-T5jnUbz4';
 
 bot.login(token);
 bot.on('ready', () => {
     console.log('Bot On');
 })
 
-let dSize = 0;
-
 bot.on('message', msg => {
-    if (msg.content == 'salve japa') {
-        dSize++;
-        msg.reply(`salve, meu pau acabou de crescer mais 1km estando com ${dSize}km de pura massa de pênis.`);
-    } else if (msg.content == 'japa status') {
-        msg.reply(`O tamanho atual do meu pênis é ${dSize}km`);
+    const arrayMessage = msg.content.split(' ');
+
+    try {
+        if (arrayMessage[0] == '-japa') {
+            if (arrayMessage.slice(1) == 'comandos') {
+                msg.reply('Lista de comandos');
+            }
+        }
+    } catch(e) {
+        console.log(e);
     }
 })
